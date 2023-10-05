7ecdc49f43e38a0ceae82703a26e3279f9f316bd
AditRizkii <adityarizkiramadhan3@gmail.com>
2023-10-05 13:58:28 +0700

    feat : change modal msg

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,31 +9,37 @@ export const jenisKredit = [
   {
     id: "A01",
     name: "Kredit Jangka Pendek",
-    desc: "Selamat anda layak mendapatkan kredit dengan jangka pendek",
+    Ucapan: "Selamat 🎉🎉.",
+    desc: "Anda dinyatakan layak mendapatkan kredit jangka pendek dengan  limit hingga 1 juta rupiah",
   },
   {
     id: "A02",
     name: "Kredit Jangka Menengah",
-    desc: "Selamat anda layak mendapatkan kredit dengan jangka menengah",
+    Ucapan: "Selamat 🎉🎉.",
+    desc: "Anda memenuhi syarat untuk mendapatkan kredit jangka menengah dengan limit hingga 5 juta rupiah",
   },
   {
     id: "A03",
     name: "Kredit Jangka Panjang",
-    desc: "Selamat anda layak mendapatkan kredit dengan jangka panjang",
+    Ucapan: "Selamat 🎉🎉.",
+    desc: "Anda memenuhi kriteria untuk mendapatkan kredit jangka panjang dengan  limit hingga 15 juta rupiah.",
   },
   {
     id: "A04",
     name: "Kredit Produktif",
-    desc: "Selamat anda layak mendapatkan kredit dengan jangka produktif",
+    Ucapan: "Selamat 🎉🎉.",
+    desc: "Anda dinyatakan layak untuk mendapatkan kredit produktif dengan limit hingga 10 juta rupiah.",
   },
   {
     id: "A05",
     name: "Kredit Konsumtif",
-    desc: "Selamat anda layak mendapatkan kredit dengan jangka konsumtif",
+    Ucapan: "Selamat 🎉🎉.",
+    desc: "Anda memenuhi syarat untuk mendapatkan kredit konsumtif dengan limit hingga 8 juta rupiah",
   },
   {
     id: "A06",
     name: "Belum Layak Mendapatkan Kredit",
-    desc: "mohon maaf anda belum layak",
+    Ucapan: "Mohon maaf 😥😥.",
+    desc: "Saat ini Anda belum memenuhi syarat untuk mendapatkan kredit. Kami akan senang membantu Anda mempersiapkan diri untuk masa depan yang lebih baik dan mencapai kelayakan mendapatkan kredit di waktu mendatang",
   },
 ];
